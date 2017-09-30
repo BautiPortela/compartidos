3f68b4e1884923e6eb3ffa43e719be4b365d19a9
sammartfrank <franksammartino7@gmail.com>
2017-09-30 16:32:27 -0300

    un poco el style bienvenido

--- php/footer.php
+++ php/footer.php
@@ -11,8 +11,9 @@
 						<a href="http:www.facebook.com"><img src="imagenes/facebook-logo.png" alt=""></a>
 						<a href="http:www.instagram.com"><img src="imagenes/instagram-logo.png" alt=""></a>
 						<a href="http:www.twitter.com"><img src="imagenes/twitter-logo.png" alt=""></a>
-					<div class="copy"><?= date(y); ?> copyright Nico;Bauti;Franck</div>
+					<div class="copy"><?=date("Y"); ?> copyright Nico;Bauti;Franck</div>
 				</div>
 			</footer>
+			<marquee behavior="scroll" direction="left">“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” // “The sea! the sea! the open sea!, The blue, the fresh, the ever free!” // “To me, the sea is like a person–like a child that I’ve known a long time. It sounds crazy, I know, but when I swim in the sea I talk to it. I never feel alone when I’m out there.” // “The ocean stirs the heart, inspires the imagination and brings eternal joy to the soul.” // “No love is Like an ocean with the dizzy procession of the waves’ boundaries …” // “The sea lives in every one of us …” // “Your heart is like the ocean, mysterious and dark.” <?= ?></marquee>
 </body>
 </html>

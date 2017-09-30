a5adf8f4d40bfce1c509d8a3035b138766cc168a
Franco Sammartino <31329000+sammartfrank@users.noreply.github.com>
2017-09-30 14:46:14 -0300

    Update footer.php
    le agrego la funcion date(year)

--- php/footer.php
+++ php/footer.php
@@ -11,7 +11,7 @@
 						<a href="http:www.facebook.com"><img src="imagenes/facebook-logo.png" alt=""></a>
 						<a href="http:www.instagram.com"><img src="imagenes/instagram-logo.png" alt=""></a>
 						<a href="http:www.twitter.com"><img src="imagenes/twitter-logo.png" alt=""></a>
-					<div class="copy">2017 copyright Nico;Bauti;Franck</div>
+					<div class="copy"><?=echo date(y); ?> copyright Nico;Bauti;Franck</div>
 				</div>
 			</footer>
 </body>

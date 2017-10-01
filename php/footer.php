cb425f8a717720597097f2be7e22a8b0def06724
sammartfrank <franksammartino7@gmail.com>
2017-09-30 22:21:45 -0300

    laburo de login.css+login.php

--- php/footer.php
+++ php/footer.php
@@ -3,17 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="css/registro.css">
+	<link rel="stylesheet" href="css/login.css">
 </head>
 <body>
+			<marquee class="marquee" behavior="scroll" direction="left">“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” // “The sea! the sea! the open sea!, The blue, the fresh, the ever free!” // “To me, the sea is like a person–like a child that I’ve known a long time. It sounds crazy, I know, but when I swim in the sea I talk to it. I never feel alone when I’m out there.” // “The ocean stirs the heart, inspires the imagination and brings eternal joy to the soul.” // “No love is Like an ocean with the dizzy procession of the waves’ boundaries …” // “The sea lives in every one of us …” // “Your heart is like the ocean, mysterious and dark.”</marquee>
 	<footer>
+				<div class="copy"><?=date("Y"); ?> copyright Nicolás ; Bautista ; Franco</div>
 				<div class="social">
-						<a href="http:www.facebook.com"><img src="imagenes/facebook-logo.png" alt=""></a>
-						<a href="http:www.instagram.com"><img src="imagenes/instagram-logo.png" alt=""></a>
-						<a href="http:www.twitter.com"><img src="imagenes/twitter-logo.png" alt=""></a>
-					<div class="copy"><?=date("Y"); ?> copyright Nico;Bauti;Franck</div>
+						<a href="?//http:www.facebook.com" target="_blank"><img src="../imagenes/facebook-logo.png" alt=""></a>
+						<a href="?http:www.instagram.com" target="_blank"><img src="../imagenes/instagram-logo.png" alt=""></a>
+						<a href="?http:www.twitter.com" target="_blank"><img src="../imagenes/twitter-logo.png" alt=""></a>
 				</div>
 			</footer>
-			<marquee behavior="scroll" direction="left">“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” // “The sea! the sea! the open sea!, The blue, the fresh, the ever free!” // “To me, the sea is like a person–like a child that I’ve known a long time. It sounds crazy, I know, but when I swim in the sea I talk to it. I never feel alone when I’m out there.” // “The ocean stirs the heart, inspires the imagination and brings eternal joy to the soul.” // “No love is Like an ocean with the dizzy procession of the waves’ boundaries …” // “The sea lives in every one of us …” // “Your heart is like the ocean, mysterious and dark.” <?= ?></marquee>
 </body>
 </html>
--- php/login.php
+++ php/login.php
@@ -1,63 +1,68 @@
-<?php
-		require_once("funciones.php");
+<?php 
+    require_once("funciones.php");
 
-		// if (estaLogueado()) {
-  	// 						header("Location:homeox.php");
-		// 				}
-		$arrayErrores = [];
+
+    /*if (estaLogueado()) {
+                header("Location:home.php");
+            }
+    */
+
 
 if ($_POST) {
 
 //Validar
-  		$arrayErrores = validarLogin($_POST);
+      $arrayErrores = validarLogin($_POST);
 
 //Si es valido, loguear
-  		if (count($arrayErrores) == 0) {
-    									loguear($_POST["email"]);
-											header("Location:../html/home.php");exit;
-										}
-										else {
-											print_r($arrayErrores);
-										}
-    	if (isset($_POST["recordame"])) {
-      									recordarUsuario($_POST["email"]);
-    									}
-  										}
+  
+      if (count($arrayErrores) == 0) {
+                      loguear($_POST["email"]);
+      if (isset($_POST["recordame"])) {
+                        recordarUsuario($_POST["email"]);
+                      }
+      header("Location:home.php");exit;
+                      }
+      }
+     
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>L o g i n || N@LU</title>
-		<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
-		<link href="http://meyerweb.com/eric/tools/css/reset/reset.css" rel="stylesheet">
-		<link rel="stylesheet" href="../css/bienvenidos.css">
+  <head>
+    <title>L o g i n || N@LU</title>
+    <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+    <link href="http://meyerweb.com/eric/tools/css/reset/reset.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta charset="UTF-8">
-	</head>
-	<body>
-	<div class="registro">
-    	<form action="" method="post">
-      		<div class="user">
-      			Email: <input type="email" name="email" value="" placeholder="Usuario">
-      		</div>
-      		<div class="pass">
-      			Contraseña: <input type="password" name="pass" value="" placeholder="Contraseña">
-      		</div>
-      		<div class="login">
-      			<button type="submit">Iniciar sesion</button>
-	      		<div class="record">
-	        		Recordarme <input type="checkbox" name="recordame" value="recordame">
-	      		</div>
-      		</div>
-    	</form>
-  	</div>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+  <header>
+    <div class="logo">
+      <a href="homeOX.php"><img src="../imagenes/flor.png" alt=""></a>
+    </div>
+  </header>
+  <div class="registrologin">
+      <form action="" method="post">
+          <div class="user">
+            Usuario: <input type="text" name="" value="" placeholder="Usuario">
+          </div>
+          <div class="pass">
+            Contraseña: <input type="password" value="" placeholder="Contraseña">
+          </div>
+          <div class="ingreso">
+            <button class="login" type="submit">Iniciar sesion</button>
+            <div class="record">
+              Recordarme <input type="checkbox" name="recordame" value="recordame">
+            </div>
+          </div>
+      </form>
+    </div>
   <div class="box">
         <div class="infopromo">
           <img src="" alt="">
-          <h1></h1>
-          <h3>Novedades y Premios</h3>
+          <h1></h1>         
+          <h3></h3>
           <p></p>
         </div>
   </div>
-
-   <?php require_once ("footer.php"); ?>
+<?php require_once ("footer.php"); ?>
